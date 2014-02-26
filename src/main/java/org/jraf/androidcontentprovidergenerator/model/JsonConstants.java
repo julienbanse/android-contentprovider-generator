96d3e2dccafaa4b06f1e250c5744ee6ca8a7fc8f
Julien Banse <julien.banse@gmail.com>
2014-02-26 18:45:04 +0100

    add missing constant file

---
+++ src/main/java/org/jraf/androidcontentprovidergenerator/model/JsonConstants.java
@@ -0,0 +1,22 @@
+package org.jraf.androidcontentprovidergenerator.model;
+
+/**
+ * Created by jbanse on 25/02/2014.
+ */
+public interface JsonConstants {
+
+    String ENTITY_TYPE = "type";
+
+    String NAME = "name";
+
+    String TYPE = "type";
+
+    String INDEX = "index";
+
+    String NULLABLE = "nullable";
+
+    String DEFAULT_VALUE = "default_value";
+
+
+
+}
